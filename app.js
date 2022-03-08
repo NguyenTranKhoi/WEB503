16999c6f9e630f7a15b815ea313290b93a01e6d3
khoi <khoidolce@gmail.com>
2022-03-08 10:50:24 +0700

    update

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-    console.log("url", req.url);
-    if (req.url === "/") {
-        res.setHeader('Content-Type', "text/html; charset=utf-8");
-        res.write("<html><body><h1>Hello lại là tôi đây</h1></body></html>")
-        res.end();
-    } else if (req.url === "/api/products") {
-        const products = [
-            {
-                id: 1,
-                name: "Admin A"
-            },
-            {
-                id: 2,
-                name: "Admin B"
-            }
-        ]
-    } else {
-        console.log("Khum bt");
-    }
-});
+const app = express();
+
+// const server = http.createServer((req, res) => {
+//     console.log("url", req.url);
+//     if (req.url === "/") {
+//         res.setHeader('Content-Type', "text/html; charset=utf-8");
+//         res.write("<html><body><h1>Hello lại là tôi đây</h1></body></html>")
+//         res.end();
+//     } else if (req.url === "/api/products") {
+//         const products = [
+//             {
+//                 id: 1,
+//                 name: "Admin A"
+//             },
+//             {
+//                 id: 2,
+//                 name: "Admin B"
+//             }
+//         ]
+//     } else {
+//         console.log("Khum bt");
+//     }
+// });
 
 const PORT = 3000;
 server.listen(PORT, () => {
