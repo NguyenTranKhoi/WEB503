3588b2b2f7c997f12a0c808b45e7022c5d1ba144
khoi <khoidolce@gmail.com>
2022-03-12 11:05:09 +0700

    feat: list product, thêm product

--- controllers/products.js
+++ controllers/products.js
@@ -1,26 +1,34 @@
-//fake data
-const products = [
-    {
-        id: 1,
-        name: "Product A"
-    },
-    {
-        id: 2,
-        name: "Product B"
-    },
-]
+import mongoose from "mongoose";
+//Khởi tạo modal
+const Product = mongoose.model("Product", { name: String });
 
+//API thêm sản phẩm
+export const create = (req, res) => {
+    try {
+        const product = await new Product(req.body).save();
+        res.json(product)
+    } catch (error) {
+        res.status(400).json({
+            message: "Không thêm được sản phẩm"
+        })
+    }
+}
+
+//API list sản phẩm
 export const list = (req, res) => {
-    res.json(products);
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (error) {
+        res.status(400).json({
+            message: "Lỗi không tìm được sản phẩm"
+        })
+    }
 }
 export const read = (req, res) => {
     res.json(products.find(item => item.id === +req.params.id));
 }
-export const create = (req, res) => {
-    // products.push(req.body);
-    const product = req.body
-    res.json(product);
-}
+
 export const remove = (req, res) => {
     res.json(products.filter(item => item.id !== +req.params.id));
 }
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import cors from 'cors';
 import productRouter from '../routes/products';
 import morgan from 'morgan';
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 //middleware
@@ -9,7 +10,15 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json())
 
+//router
 app.use("/api", productRouter);
+
+//connect database
+mongoose.connect("mongodb://localhost:27017/we16309")
+    .then(() => console.log("Kết nối đb thành công"))
+    .catch((error) => console.log(error))
+
+//connection
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log("Server is running port", PORT);
