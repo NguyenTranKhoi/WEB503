36e68a3b23ab3837bd4766c715e873b3100f35e8
khoi <khoidolce@gmail.com>
2022-03-12 10:17:22 +0700

    feat: product controller

--- src/app.js
+++ src/app.js
@@ -5,40 +5,11 @@ import express from 'express';
 
 const app = express();
 //middleware
-
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json())
 
-app.use(productRouter);
-
-// app.use(check)
-// app.use((req, res) => {
-//     console.log("Bước 2");
-// })
-
-// const server = http.createServer((req, res) => {
-//     console.log("url", req.url);
-//     if (req.url === "/") {
-//         res.setHeader('Content-Type', "text/html; charset=utf-8");
-//         res.write("<html><body><h1>Hello lại là tôi đây</h1></body></html>")
-//         res.end();
-//     } else if (req.url === "/api/products") {
-//         const products = [
-//             {
-//                 id: 1,
-//                 name: "Admin A"
-//             },
-//             {
-//                 id: 2,
-//                 name: "Admin B"
-//             }
-//         ]
-//     } else {
-//         console.log("Khum bt");
-//     }
-// });
-
+app.use("/api", productRouter);
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log("Server is running port", PORT);
