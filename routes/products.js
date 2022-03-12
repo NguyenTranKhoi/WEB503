144ba62fc1acf23d1a609b13b9f0808ff9a99f6b
khoi <khoidolce@gmail.com>
2022-03-12 10:13:44 +0700

    feat: product controller

--- routes/products.js
+++ routes/products.js
@@ -1,34 +1,13 @@
 import { Router } from 'express';
+import { list, read, remove, update } from '../controllers/products';
 import { checkAuth, chekAuth } from '../middlewares/checkAuth'
 const router = Router();
-//fake data
-const product = [
-    {
-        id: 1,
-        name: "Product A"
-    },
-    {
-        id: 2,
-        name: "Product B"
-    },
-]
 
-router.get("/products", checkAuth, (req, res) => {
-    res.json(products);
-});
-
-
-router.get("/products/:id", checkAuth, (req, res) => {
-    res.json(products.find(item => item.id === +req.params.id));
-});
-
-router.post("/products", checkAuth, (req, res) => {
-    products.push(req.body);
-    res.json(products);
-});
-
-router.delete("/products:id", checkAuth, (req, res) => {
-    res.json(products.filter(item => item.id !== +req.params.id));
-});
+//resful API
+router.get("/products", checkAuth, list);
+router.get("/products/:id", checkAuth, read);
+router.post("/products", checkAuth,);
+router.delete("/products:id", checkAuth, remove);
+router.put("/products:id", checkAuth, update);
 
 export default router;
