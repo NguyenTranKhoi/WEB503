d8bfb427090aa4e784b6638ccf705ff158563d74
khoi <khoidolce@gmail.com>
2022-04-02 09:11:25 +0700

    feact: product errors

--- controllers/products.js
+++ controllers/products.js
@@ -10,6 +10,7 @@ export const create = async (req, res) => {
         console.log('product', product);
         res.json(product)
     } catch (error) {
+        console.log(error);
         res.status(400).json({
             message: "Không thêm được sản phẩm"
         })
