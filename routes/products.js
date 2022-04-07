a5457c54fc5d1eebe43fcd2029698cdeccc8ea47
khoi <khoidolce@gmail.com>
2022-04-08 00:17:10 +0700

    updatye

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 import { create, list, read, remove, update } from '../controllers/products';
 import { checkAuth, isAdmin, isAuth, reqireSignin } from '../middlewares/checkAuth';
-import { lists } from '../controllers/price';
-import { listPosts } from '../controllers/posts';
 import { userById } from '../controllers/user';
 import { render } from 'express/lib/response';
 const router = Router();
@@ -15,9 +13,6 @@ router.post("/products/:userId", create);
 router.param("userId", userById);
 router.delete("/products/:id", checkAuth, remove);
 router.put("/products/:id", checkAuth, update);
-//router price
-router.get("/price", checkAuth, lists);
-//router posts
-router.get("/posts", checkAuth, listPosts);
+
 
 export default router;
